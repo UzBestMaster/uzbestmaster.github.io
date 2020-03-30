7eecebfc075f0a88b552aed96eaa938c0ce48c8a
Elbek <62874603+UzBestMaster@users.noreply.github.com>
2020-03-30 14:48:53 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+	<meta property="og:title" content="Info.E-soft.Uz">
+	<meta property="og:description" content="Hamdullayev Elbek AKmal o'g'li - Portifolio">
+	<meta property="og:image" content="https://info.e-soft.uz/assets/img/e-soft.jpg">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Info.E-soft.uz - Hamdullayev Elbek Akmal o'g'li</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">
+    <img src="assets/img/e-soft.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
+    Hamdullayev Elbek Akmal o'g'li
+  </a>
+</nav>
+    <div class="container">
+    <div class="row">
+    <div class="col-sm-8"><br>
+      <div class="card border-primary mb-3">
+      <div class="card-header">Portifolio</div>
+      <div class="card-body text-primary">
+        <h5 class="card-title">Hamdullayev Elbek Akmal o'g'li</h5>
+        <p class="card-text">O'rganuvchi dasturchi</p>
+		<p class="card-text">Men Hamdullayev Elbek Akmal o'g'li Samarqand viloyati, Kattaqo'rg'on tumani 23-umumiy o'rta ta'lim maktabining 9-sinfida o'qiyman.
+		12 yoshligimdan kompyuterga qiziqishim orta boshlagan. Avval kompyuterda ishlashni, so'ng Office dasturlarini o'rganib chiqdim. 13 yoshimda(2017-yil) birinchi
+		windows dasturimni Delphida tuzdim. Birinchi dasturim Elektron soat dasturi edi. 14 yoshimda C++ dasturlash tilini o'rganishni boshladim va hozirda
+		o'rganishni davom ettiryapman. Bu yillar orasida web-dasturlash, delphi, pascal, androidga dasturlashga ham qiziqib ko'rdim. Sizlarga ham dasturlashni o'rganishni
+		tafsiya qilaman. Mening <a href="https://www.e-soft.uz">E-soft.Uz</a> saytimda dasturlashni o'rganing! O'zim bilganlarimni sizga ham o'rgatishdan xursand bo'lar edim !!!</p>
+      </div>
+    </div>
+    </div>
+    <div class="col-sm-4"><br>
+      <div class="card border-primary mb-3">
+      <div class="card-header">Bog'lanish</div>
+      <div class="card-body text-primary">
+        <h5 class="card-title">Web-sayt: <a href="https://www.e-soft.uz" target="_blank">https://www.e-soft.uz</a></h5>
+        <p class="card-text">Elektron pochta: <a href="mailto:elbek-h@umail.uz">elbek-h@umail.uz</a></p>
+        <p class="card-text">Telegram [kanal]: <a href="https://t.me/s/ESoftUz" target="_blank">@ESoftUz</a></p>
+        <p class="card-text">Telegram [guruh]: <a href="https://t.me/ESoftUzGroup" target="_blank">@ESoftUzGroup</a></p>
+        <p class="card-text">Telegram [profil]: <a href="https://t.me/ESoftUzb" target="_blank">@ESoftUzb</a></p>
+      </div>
+    </div>  
+  <div class="card border-primary mb-3">
+  <div class="card-header">Qiziqishi</div>
+  <div class="card-body text-primary">
+    <h5 class="card-title">Dasturlash</h5>
+    <p class="card-text">Kompyuter</p>
+    <p class="card-text">Informatika</p>
+    <p class="card-text">Elektronika</p>
+    <p class="card-text">Informatsion texnologiyalar</p>
+  </div>
+</div>
+    <div class="card border-primary mb-3">
+  <div class="card-header">Ta'lim</div>
+  <div class="card-body text-primary">
+    <h5 class="card-title">Samarqand viloyati, Kattaqo'rg'on tumani</h5>
+    <p class="card-text">23-umumiy o'rta ta'lim maktabi 9-sinf o'quvchisi</p>
+  </div>
+</div>
+</div>
+    </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
